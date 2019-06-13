ba25c8b88871eaa00aab40518e1db16a430eb5b1
luisgr95 <37317648+luisgr95@users.noreply.github.com>
2019-06-13 15:15:56 -0600

    Euler7
    C# Visual Studio Command Line

---
+++ Euler 7/Euler 7/Divisiontrial.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler_7
+{
+    class Divisiontrial
+    {
+        public static bool primo(int numm)
+        {
+            if (numm <= 1)
+            {
+                return false;
+            }
+
+            if (numm == 2)
+            {
+                return true;
+            }
+
+            if (numm % 2 == 0)
+            {
+                return false;
+            }
+
+            int counter = 3;
+
+            while ((counter * counter) <= numm)
+            {
+                if (numm % counter == 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    counter += 2;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Euler 7/Euler 7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numPrimos = 1;
+            int numm = 1;
+            int pasos = 10001;
+            while (numPrimos < pasos)
+            {
+                numm = numm + 2;
+                if (Divisiontrial.primo(numm))
+                {
+                    numPrimos++;
+                }
+            }
+            Console.WriteLine("El numero de {0}st su primo es {1}", pasos, numm); 
+            Console.ReadKey();
+        } 
+    }
+}
